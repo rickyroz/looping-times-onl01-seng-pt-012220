732b7352d93b96af433f7a31f2e65da75c7ccdaf
Ricky Rozier <rickydrozier@gmail.com>
2020-02-02 04:47:54 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,16 +1,3 @@
-def 
-  jewels_in_bag = 100
- 
-3.times do 
-  puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10
-  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
-end
-# => 3 (return value)
- 
-puts "We have #{jewels_in_bag} jewels still to hide!"
-end
-
 
 
 
